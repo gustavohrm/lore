85f4e433fd105bba1c94b329725d39c4293ba883
Gustavo <gustavo@coden.agency>
2026-01-01 18:31:54 -0300

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  root: "./src",
+  publicDir: "./_public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "./src/index.html",
+      },
+    },
+  },
+  plugins: [tailwindcss()],
+});
